cd6249dbb3c9ab57c7522dfee59406ffb286b86e
joshhrycek <joshhrycek@gmail.com>
2019-09-05 14:31:50 -0400

    basic component playground

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Split from './composition/Split';
+import './App.css'
+
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <Split className='left' flexBasis={23}>
+        blah BLAH BLAH BLAH 
+      </Split>
+      <Split className='right'>
+        pomf POMF POMF PMF POMF [MFASDF]
+      </Split>
     </main>
   );
 }
